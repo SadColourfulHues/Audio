80a9989fa459736ffb465efb1ca219375cf7ab2c
SadColourfulHues <46611929+SadColourfulHues@users.noreply.github.com>
2024-04-14 10:16:08 +0800

    (feat) [MusicPlayer] Added beat/bar watcher + elapsed events

--- Core/MusicPlayer.cs
+++ Core/MusicPlayer.cs
@@ -1,5 +1,6 @@
 using Godot;
 
+using System;
 using System.Diagnostics;
 
 namespace SadChromaLib.Audio;
@@ -10,9 +11,32 @@ namespace SadChromaLib.Audio;
 [GlobalClass]
 public sealed partial class MusicPlayer: Node
 {
+    /// <summary>
+    /// If 'Watch for Beats' is enabled, this event is triggered whenever a beat
+    /// has elapsed during playback.
+    ///
+    /// <para>
+    ///     OnBeatTick: (int) inBarBeat, (int) totalBeat
+    /// </para>
+    /// </summary>
+    public event Action<int, int> OnBeatTick;
+
+    /// <summary>
+    /// If 'Watch for Beats' is enabled, this event is triggered whenever a bar
+    /// has elapsed during playback.
+    ///
+    /// <para>
+    ///     OnBarTick: (int) barIdx
+    /// </para>
+    /// </summary>
+    public event Action<int> OnBarTick;
+
     [ExportGroup("Setup")]
     [Export]
     MusicLibrary _library;
+
+    [Export]
+    bool _watchForBeats;
 
     [ExportGroup("Crossfade")]
     [Export]
@@ -29,7 +53,12 @@ public sealed partial class MusicPlayer: Node
     string _currentBgmId;
     string _targetBgmId;
 
+    double _bps;
+    int _barBeats;
     float _checkTick;
+
+    int _lastBeat;
+    int _lastBar;
 
     public override void _Ready()
     {
@@ -37,6 +66,9 @@ public sealed partial class MusicPlayer: Node
         _isFading = false;
         _currentBgmId = null;
         _targetBgmId = null;
+
+        _bps = 0.0;
+        _barBeats = 4;
 
         Debug.Assert(
             condition: _library is not null,
@@ -51,26 +83,18 @@ public sealed partial class MusicPlayer: Node
 
         AddChild(_sourcePlayer);
         AddChild(_fadePlayer);
-
-        SetProcess(false);
     }
 
-    public override void _Process(double delta)
+    public override void _Process(double deltaD)
     {
-        _checkTick += (float) delta;
+        float delta = (float) deltaD;
 
-        if (_checkTick < _trackTransitionTickThreshold)
+        WatchForTransitionRequest(delta);
+
+        if (!_watchForBeats)
             return;
 
-        _checkTick = 0.0f;
-
-        if (_targetBgmId is null || _currentBgmId == _targetBgmId)
-            return;
-
-        StartFade();
-        SetStream(_targetBgmId, _sourcePlayer);
-
-        _currentBgmId = _targetBgmId;
+        WatchForBeats(delta);
     }
 
     #region Main Functions
@@ -86,7 +110,49 @@ public sealed partial class MusicPlayer: Node
         }
 
         _targetBgmId = bgmId;
-        SetProcess(true);
+    }
+
+    #endregion
+
+    #region Watchers
+
+    private void WatchForTransitionRequest(float delta)
+    {
+        _checkTick += delta;
+
+        if (_checkTick < _trackTransitionTickThreshold)
+            return;
+
+        _checkTick = 0.0f;
+
+        if (_targetBgmId is null || _currentBgmId == _targetBgmId)
+            return;
+
+        StartFade();
+        SetStream(_targetBgmId, _sourcePlayer);
+
+        _currentBgmId = _targetBgmId;
+    }
+
+    private void WatchForBeats(float delta)
+    {
+        if (_bps < 0.5f || !_sourcePlayer.Playing)
+            return;
+
+        double position = _sourcePlayer.GetPlaybackPosition();
+
+        int estimatedBeatIdx = Mathf.FloorToInt(position * _bps);
+        int estimatedBar = Mathf.FloorToInt((float) estimatedBeatIdx / _barBeats);
+
+        if (estimatedBeatIdx != _lastBeat) {
+            OnBeatTick?.Invoke(estimatedBeatIdx % _barBeats, estimatedBeatIdx);
+            _lastBeat = estimatedBeatIdx;
+        }
+
+        if (estimatedBar != _lastBar) {
+            OnBarTick?.Invoke(estimatedBar);
+            _lastBar = estimatedBar;
+        }
     }
 
     #endregion
@@ -95,11 +161,38 @@ public sealed partial class MusicPlayer: Node
 
     private void SetStream(string id, AudioStreamPlayer player)
     {
+        const double BpsFac = 1.0 / 60.0;
+
         if (!_library.TryGet(id, out BgmTrack track))
             return;
 
         player.Stream = track.Stream;
         player.Play();
+
+        if (!_watchForBeats)
+            return;
+
+        _lastBar = 0;
+        _lastBeat = 0;
+
+        // Update track info
+        switch (track.Stream)
+        {
+            case AudioStreamMP3 streamMp3:
+                _bps = streamMp3.Bpm * BpsFac;
+                _barBeats = streamMp3.BarBeats;
+                break;
+
+            case AudioStreamOggVorbis streamOgg:
+                _bps = streamOgg.Bpm * BpsFac;
+                _barBeats = streamOgg.BarBeats;
+                break;
+
+            default:
+                _bps = 0.0f;
+                _barBeats = 4;
+                break;
+        }
     }
 
     private void TweenFadeCallback(float f)
